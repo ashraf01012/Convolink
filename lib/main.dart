5eb95c2b3ffe25d31133cc4cd57486b741965bf1
omarhefnawy <150110202+omarhefnawy@users.noreply.github.com>
2024-04-07 19:46:06 +0200

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'features/auth/login/view/page/login_page.dart';
 
 void main() {
-
+//all of us did that omar hefnawy mohamed , ashraf abdo kamel 
   MaterialApp materialApp = const MaterialApp(
       debugShowCheckedModeBanner: false,
       home: LogInPage(),

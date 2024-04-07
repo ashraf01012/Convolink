7b32c6b60b96792a1b776c25071e29b14db16ca3
omarhefnawy <150110202+omarhefnawy@users.noreply.github.com>
2024-04-07 19:48:00 +0200

    Update validation.dart

--- lib/core/utils/validation.dart
+++ lib/core/utils/validation.dart
@@ -13,13 +13,13 @@ class ChatValidation{
     if((pass?.length??0)>=12){
       return null;
     }
-    return 'invalid password';
+    return 'Invalid Password';
   }
   String? emailValidate(String? mail){
     String source=r"^(\w){5,15}@gmail.com";
     RegExp regExp=RegExp(source);
     bool accept =regExp.hasMatch(mail??'');
     if(accept)return null;
-    return 'invalid email';
+    return 'Invalid Email';
   }
 }
